75600f7a741f243a8cec6ea5b77a6a963999b201
jowaSpaghetti <114553075+jowaSpaghetti@users.noreply.github.com>
2023-06-12 20:06:17 +0800

    complete DormDriver code
     - without commented-out helper functions
    - added dorm name input validation at Dormitory constructor

--- Dormitory.java
+++ Dormitory.java
@@ -7,7 +7,9 @@ public class Dormitory {
 	public Dormitory(String name, int numberOfRooms, int maxRoomCapacity) {
 		int i;
 		
-		if(	numberOfRooms > 0 &&
+		if(	name != null &&
+			name.length() > 0 &&
+			numberOfRooms > 0 &&
 			maxRoomCapacity > 0 ) {
 			this.name = name;
 			this.numberOfRooms = numberOfRooms;
@@ -54,62 +56,6 @@ public class Dormitory {
 		return maxRoomCapacity;
 	}
 	
-	/*
-	public void removeGuest(Person guest) {
-		int i;
-		int j;
-		for( i = 0; i < rooms.length; i++ ) {
-			if( rooms[i].isEmpty() == false ) 
-				for( j = 0; j < rooms[i].getMaxCapacity(); j++ ) 
-					if( rooms[i].getGuests()[j] == guest)
-						break;
-			if( j < rooms[i].getMaxCapacity() ) {
-				rooms[i].removeGuest(j);
-				break;
-			}
-		}
-	}
-	*/
-	
-	/**
-	
-		@return room number of the first unoccupied room in the room array, -1 if all rooms are full
-	*/
-	/*
-	public int findUnoccupiedRoom() {
-		int i;
-		
-		i = 0;
-		while( i < rooms.length && rooms[i].isEmpty() == false )
-			i++;
-		
-		if( i < rooms.length )
-			return rooms[i].getRoomNum();
-		else
-			return -1;
-	}
-	*/
-	
-	
-	/**
-	
-		@return room number of the guest, -1 if guest not found
-	*/
-	/*
-	public int findRoomOfGuest(Person guest) {
-		int i;
-		int j;
-		Person[] guests;
-		
-		for( i = 0; i < rooms.length; i++ ) {
-			guests = rooms[i].getGuests();
-			for( j = 0; j < guests.length; j++ )
-				if( guests[j] == guest )
-					return rooms[i].getRoomNum();
-		}
-		return -1;
-	}
-	*/
 
 	
 	private String name; // dormitory's name
